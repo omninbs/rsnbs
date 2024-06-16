8ac04ab7f61510853adedf6c14d8900400949c29
John-Bush14 <joppe@stuyck.be>
2024-06-16 20:30:06 +0200

    feat: NoteBlockSong field iteration

--- src/nbs_class.rs
+++ src/nbs_class.rs
@@ -1,3 +1,5 @@
+extern crate paste;
+
 pub struct NoteBlockSong {
     header: NoteBlockSongHeader,
 
@@ -9,52 +11,97 @@ pub struct NoteBlockSong {
     instruments: Vec<NoteBlockSongInstrument>
 }
 
-struct NoteBlockSongHeader {
-    version: Option<i8>,
-    default_instruments: Option<i8>,
-    song_length: Option<i16>,
-    song_layers: i16,
-    song_name: String,
-    song_author: String,
-    original_author: String,
-    description: String,
-    tempo: i16,
-    auto_save: bool,
-    auto_saving_duration: i8,
-    time_signature: i8,
-    minutes_spent: i32,
-    left_clicks: i32,
-    right_clicks: i32,
-    blocks_added: i32,
-    blocks_removed: i32,
-    og_file: String,
-    r#loop: Option<bool>,
-    max_loop_count: Option<i8>,
-    loop_start: Option<i16>
+macro_rules! create_iterable_struct {
+    ($struct_name:ident, [$(($field:ident, $type:ty, $version:expr)),*]) => {
+        // Define the struct
+        struct $struct_name {
+            $(
+                $field: Option<$type>,
+            )*
+        }
+
+        // Define an enum to hold references to the fields
+        paste::item! {
+            enum [<$struct_name FieldRef>]<'a> {
+                $(
+                    [< $field:camel >](&'a Option<$type>, i8),
+                )*
+            }
+        }
+
+        // Implement the iterator function for the struct
+        impl $struct_name {
+            paste::item! {
+                fn iter(&self) -> impl Iterator<Item = [<$struct_name FieldRef>]<'_>> {
+                    vec![
+                        $(
+                            [<$struct_name FieldRef>]::[< $field:camel >](&self.$field, $version),
+                        )*
+                    ].into_iter()
+                }
+            }
+        }
+    };
 }
 
-struct NoteBlockSongNote {
-    tick: i16,
-    layer: i16,
-    instrument: i8,
-    key: i8,
-    velocity: Option<i8>,
-    panning: Option<u8>,
-    pitch: Option<i16>
-}
+create_iterable_struct!(
+    NoteBlockSongHeader,
+    [
+        (version, i8, 1),
+        (default_instruments, i8, 1),
+        (song_length, i16, 3),
+        (song_layers, i16, 0),
+        (song_name, String, 0),
+        (song_author, String, 0),
+        (original_author, String, 0),
+        (description, String, 0),
+        (tempo, i16, 0),
+        (auto_save, bool, 0),
+        (auto_saving_duration, i8, 0),
+        (time_signature, i8, 0),
+        (minutes_spent, i32, 0),
+        (left_clicks, i32, 0),
+        (right_clicks, i32, 0),
+        (blocks_added, i32, 0),
+        (blocks_removed, i32, 0),
+        (og_file, String, 0),
+        (r#loop, bool, 4),
+        (max_loop_count, i8, 4),
+        (loop_start, i16, 4)
+    ]
+);
 
-struct NoteBlockSongLayer {
-    id: i16,
-    name: String,
-    lock: Option<bool>,
-    volume: Option<i8>,
-    panning: Option<u8>
-}
+create_iterable_struct!(
+    NoteBlockSongNote,
+    [
+        (tick, i16, 0),
+        (layer, i16, 0),
+        (instrument, i8, 0),
+        (key, i8, 0),
+        (velocity, i8, 4),
+        (panning, u8, 4),
+        (pitch, i16, 4)
+    ]
+);
 
-struct NoteBlockSongInstrument {
-    id: i16,
-    name: String,
-    file: String,
-    pitch: i8,
-    press_key: bool
-}
+create_iterable_struct!(
+    NoteBlockSongLayer,
+    [
+        (id, i16, 127), // don't read/write only set as struct property
+        (name, String, 0),
+        (lock, bool, 4),
+        (volume, i8, 0),
+        (stereo, u8, 2)
+    ]
+);
+
+create_iterable_struct!(
+    NoteBlockSongInstrument,
+    [
+        (id, i16, 127),
+        (name, String, 0),
+        (file, String, 0),
+        (pitch, i8, 0),
+        (press_key, bool, 0)
+    ]
+);
