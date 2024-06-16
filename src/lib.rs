b7d57a1642d3aa2445c41d28bb1db862a6ab3e4b
John-Bush14 <joppe@stuyck.be>
2024-06-16 22:43:55 +0200

    setup: lib layout

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
 mod nbs_class;
 use nbs_class::NoteBlockSong;
+
+pub fn read(file: &str) -> Result<NoteBlockSong, io::Error> {todo!()}
+
+pub fn save(nbs: NoteBlockSong, version: u8) -> Result<(), io::Error> {todo!()}
+
+pub fn new(name: &str) -> NoteBlockSong {todo!()}
