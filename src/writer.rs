4a409d84a8bcc6dafe8e129c66ec08f4fe07518c
John-Bush14 <joppe@stuyck.be>
2024-06-22 21:09:21 +0200

    feat completion: file saving

--- src/writer.rs
+++ src/writer.rs
@@ -1,13 +1,14 @@
 use super::{Song, Binary};
 use std::fs::File;
 use std::io::Write;
+use std::fs::OpenOptions;
 
 macro_rules! impl_writable_bin {
     ([$($int:ty,)*]) => {
         $(
             impl writable_bin for $int {
                 fn write_bin(&self, mut file: File) -> Option<()> {
-                    file.write_all(&self.to_le_bytes()).ok()?;
+                    match file.write_all(&self.to_le_bytes()) {Ok(x) => x, Err(err) => panic!("{}, {:?}", err, file)};
                     return Some(())
                 }
 
@@ -72,12 +73,12 @@ fn write_field<T: writable_bin>(file: File, field: &Option<T>) -> Option<()> {
 fn write_part(mut file: File, part: Vec<(Binary, u8)>) -> Option<()> {
     for (binary, version) in part {
         match binary {
-            Binary::Bool(val) => write_field(file.try_clone().ok()?, val),
-            Binary::Byte(val) => write_field(file.try_clone().ok()?, val),
-            Binary::UByte(val) => write_field(file.try_clone().ok()?, val),
-            Binary::Short(val) => write_field(file.try_clone().ok()?, val),
-            Binary::Integer(val) => write_field(file.try_clone().ok()?, val),
-            Binary::String(val) => write_field(file.try_clone().ok()?, val),
+            Binary::Bool(val) => write_field(file.try_clone().ok()?, val)?,
+            Binary::Byte(val) => write_field(file.try_clone().ok()?, val)?,
+            Binary::UByte(val) => write_field(file.try_clone().ok()?, val)?,
+            Binary::Short(val) => write_field(file.try_clone().ok()?, val)?,
+            Binary::Integer(val) => write_field(file.try_clone().ok()?, val)?,
+            Binary::String(val) => write_field(file.try_clone().ok()?, val)?,
         };
     }
 
@@ -85,8 +86,48 @@ fn write_part(mut file: File, part: Vec<(Binary, u8)>) -> Option<()> {
 }
 
 impl Song {
-    pub fn save(&self, file: &str, version: u8) -> Option<()> {
-        let mut file = match File::open(file) {Ok(file) => file, Err(_err) => {File::create(file); File::open(file).ok()?}};
+    pub fn save(&self, filename: &str, version: u8) -> Option<()> {
+        let mut file = match OpenOptions::new().write(true).open(filename) {
+            Ok(file) => file, 
+            Err(_err) => {File::create(filename).ok()?; File::open(filename).ok()?}
+        };
+
+        write_part(file.try_clone().ok()?, self.header.as_ref_vec());
+        
+        let mut prev_tick = -1;
+        let mut prev_layer = -1;
+        
+        for note in &self.notes {
+            if note.tick? - prev_tick > 0 {
+                if prev_tick > -1 {write_field(file.try_clone().ok()?, &Some(0i16));}
+                write_field(file.try_clone().ok()?, &Some((note.tick? - prev_tick) as i16));
+                prev_layer = -1;
+            }
+            
+            write_field(file.try_clone().ok()?, &Some((note.layer? - prev_layer) as i16));
+            
+            write_field(file.try_clone().ok()?, &note.instrument);
+            write_field(file.try_clone().ok()?, &note.key);
+            write_field(file.try_clone().ok()?, &note.velocity);
+            write_field(file.try_clone().ok()?, &note.panning);
+            write_field(file.try_clone().ok()?, &note.pitch);
+
+            prev_tick = note.tick?;
+            prev_layer = note.layer?;
+        }
+
+        write_field(file.try_clone().ok()?, &Some(0i16));
+        write_field(file.try_clone().ok()?, &Some(0i16));
+
+        for layer in &self.layers {
+            write_part(file.try_clone().ok()?, layer.as_ref_vec());
+        }
+        
+        write_field(file.try_clone().ok()?, &Some(self.instruments.len() as u8));
+
+        for instrument in &self.instruments {
+            write_part(file.try_clone().ok()?, instrument.as_ref_vec());
+        }
 
         return Some(())
     }
